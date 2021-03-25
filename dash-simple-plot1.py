adb34e2cf6258fd8200f3d38d5654331818a919a
Md Abdullah Al Mashud <almashud137@gmail.com>
2021-03-25 17:09:43 +0600

    dashboard styling

--- dash-simple-plot1.py
+++ dash-simple-plot1.py
@@ -6,12 +6,24 @@ import plotly.express as px
 df = px.data.iris()
 fig = px.scatter(df,x="sepal_width",y="sepal_length",size="petal_length",color="species")
 
-app = dash.Dash()
+fig.update_layout(
+    plot_bgcolor="#252262",
+    font_color="rgb(122,3,120)"
+)
+
+app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.H1("hello from dash app"),
-    dcc.Graph(figure=fig)
-])
+    html.H1("hello from dash app with styling",style=   {"color":"#29B1D3",
+        "textAlign":"center",
+    }
+    
+    ),
+    dcc.Graph(figure=fig,style={"height":400}
+    ),
+],
+className="dash-first-div"
+)
 
 if __name__ == "__main__":
-    app.run_server()
+    app.run_server(debug=True)
